4b5cac148ee8c0be058042893bc0c7da29cbf278
sachinkumar121 <skarora7894@gmail.com>
2023-04-16 10:57:41 +0530

    add CORS and use graphqlcli way in test.js

--- test.js
+++ test.js
@@ -1,8 +1,10 @@
 import express from "express";
+import cors from "cors";
 import { buildSchema } from "graphql";
 import { graphqlHTTP } from "express-graphql";
 
 const app = express();
+app.use(cors());
 
 app.use('/graphql', graphqlHTTP({
     schema: buildSchema(`
@@ -23,6 +25,7 @@ app.use('/graphql', graphqlHTTP({
             id: Int
             name: String
             age: Int
+            about: String
         }
         enum SortType {
             Date
@@ -31,7 +34,7 @@ app.use('/graphql', graphqlHTTP({
     `),
     rootValue: {
         hello: () => 'Hello World!',
-        me: () => ({'id': 121, 'name': 'Sachin Kumar', age: 23}),
+        me: () => ({'id': 121, 'name': 'Sachin Kumar', age: 23, about: "My name is Sachin Kumar and I am a full-stack Web Application Developer and Software Developer, currently living in Ambala, Haryana(India). I have a Master of Computer Science from Kurukshetra University, and my primary focus and inspiration for my studies are Web Development. I am both driven and self-motivated, and I am constantly experimenting with new technologies and techniques. I am very passionate about Web Development and strive to better myself as a developer, and the development community as a whole."}),
         user: ({id}) => ({id, 'name': 'Search Kumar', age: 23}),
         allUsers: ({sort}) => {
             console.log(`sort by ${sort}`);
